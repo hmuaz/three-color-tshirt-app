eab08ba4ff5d1e43fa4399df81948c7d6b719aaa
hmuaz <muazsenol1@gmail.com>
2023-07-18 14:43:21 +0300

    ai

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,6 +56,20 @@ const Customizer = () => {
     if (!prompt) return alert("Please enter a prompt");
 
     try {
+      setGeneratingImg(true);
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/ json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
       alert(error);
     } finally {
@@ -81,9 +95,11 @@ const Customizer = () => {
         break;
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName];
+        break;
       default:
         state.isFullTexture = true;
         state.isLogoTexture = false;
+        break;
     }
 
     setActiveFilterTab((prevState) => {
